36b0d65c260d411bbed7b7da1c6ba3caf81de94e
munio7 <maciej.wrotniak03@gmail.com>
2025-04-09 14:25:29 +0200

    Added navigation between Home and Explore

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/explore',
+      name: 'explore',
+      component: () => import('../views/ExploreView.vue'),
     }
   ],
 })
---
+++ src/views/ExploreView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hello explore!</h1>
+</template>
+
+
+<script setup lang="ts">
+</script>
+<style scoped></style>
